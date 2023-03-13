740da107c9d67ee3e438e8b1fb72e9fd196e884a
alvin.tan <alvintan95@gmail.com>
2023-03-13 20:32:47 +0800

    add pp

---
+++ static/pp.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cp index.html hashing/md5.html
+cp index.html hashing/SHA-256.html
+cp index.html hashing/bcrypt.html
+cp index.html datetime/timestamp.html
+cp index.html datetime/formatter.html
